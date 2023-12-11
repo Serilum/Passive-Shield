22a5a8f4e5ff3320e2c7fe0a74dcd3d07e306b15
Rick South <rick@natamus.com>
2023-12-12 00:35:07 +0100

    Passive Shield source code for 1.19.4.

---
+++ Common/src/main/java/com/natamus/passiveshield/util/Reference.java
@@ -0,0 +1,8 @@
+package com.natamus.passiveshield.util;
+
+public class Reference {
+	public static final String MOD_ID = "passiveshield";
+	public static final String NAME = "Passive Shield";
+	public static final String VERSION = "3.2";
+	public static final String ACCEPTED_VERSIONS = "[1.19.4]";
+}
