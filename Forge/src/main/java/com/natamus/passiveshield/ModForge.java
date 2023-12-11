ffd1eb3f7ec0213ffc9d63e05758943b27ad679b
Rick South <rick@natamus.com>
2023-12-12 00:35:28 +0100

    Passive Shield source code for 1.20.3.

---
+++ Forge/src/main/java/com/natamus/passiveshield/ModForge.java
@@ -0,0 +1,42 @@
+package com.natamus.passiveshield;
+
+import com.natamus.collective.check.RegisterMod;
+import com.natamus.passiveshield.forge.config.IntegrateForgeConfig;
+import com.natamus.passiveshield.forge.events.ForgeClientEvent;
+import com.natamus.passiveshield.forge.events.ForgeServerEvent;
+import com.natamus.passiveshield.util.Reference;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLEnvironment;
+
+@Mod(Reference.MOD_ID)
+public class ModForge {
+	
+	public ModForge() {
+		IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+		modEventBus.addListener(this::loadComplete);
+
+		setGlobalConstants();
+		ModCommon.init();
+
+		IntegrateForgeConfig.registerScreen(ModLoadingContext.get());
+
+		RegisterMod.register(Reference.NAME, Reference.MOD_ID, Reference.VERSION, Reference.ACCEPTED_VERSIONS);
+	}
+
+	private void loadComplete(final FMLLoadCompleteEvent event) {
+		if (FMLEnvironment.dist.equals(Dist.CLIENT)) {
+			MinecraftForge.EVENT_BUS.register(new ForgeClientEvent());
+		}
+    	MinecraftForge.EVENT_BUS.register(new ForgeServerEvent());
+	}
+
+	private static void setGlobalConstants() {
+
+	}
+}
