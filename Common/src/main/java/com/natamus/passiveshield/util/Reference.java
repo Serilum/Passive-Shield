5c7cb13e3bb3d10aebb4f82b9fc96b503d9d2789
Rick South <rick@natamus.com>
2023-12-18 10:14:29 +0100

    (v3.4) Improved mod icon compression, which lowers the jar file size. [+2]

--- Common/src/main/java/com/natamus/passiveshield/util/Reference.java
+++ Common/src/main/java/com/natamus/passiveshield/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.passiveshield.util;
 public class Reference {
 	public static final String MOD_ID = "passiveshield";
 	public static final String NAME = "Passive Shield";
-	public static final String VERSION = "3.3";
+	public static final String VERSION = "3.4";
 	public static final String ACCEPTED_VERSIONS = "[1.19.2]";
 }
