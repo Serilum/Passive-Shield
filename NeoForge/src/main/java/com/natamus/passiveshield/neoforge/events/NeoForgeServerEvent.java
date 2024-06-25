4853cb111a446edadfaca9b90c374a983d52c929
Rick South <rick@natamus.com>
2024-06-26 00:01:57 +0200

    (v3.5) (1.21.0 Neo) Updated mod to work with NeoForge version 21.0.31+.

--- NeoForge/src/main/java/com/natamus/passiveshield/neoforge/events/NeoForgeServerEvent.java
+++ NeoForge/src/main/java/com/natamus/passiveshield/neoforge/events/NeoForgeServerEvent.java
@@ -2,14 +2,14 @@ package com.natamus.passiveshield.neoforge.events;
 
 import com.natamus.passiveshield.events.ServerEvent;
 import net.minecraft.world.entity.Entity;
-import net.neoforged.neoforge.event.entity.living.LivingHurtEvent;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
+import net.neoforged.neoforge.event.entity.living.LivingIncomingDamageEvent;
 
 @EventBusSubscriber
 public class NeoForgeServerEvent {
 	@SubscribeEvent
-	public static void onEntityDamageTaken(LivingHurtEvent e) {
+	public static void onEntityDamageTaken(LivingIncomingDamageEvent e) {
 		Entity entity = e.getEntity();
 
 		float damageAmount = e.getAmount();
