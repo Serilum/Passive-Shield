8e83a79e3ddaa685c27aac0b9155fe4df38437f8
Rick South <rick@natamus.com>
2024-12-06 13:20:05 +0100

    Passive Shield source code for 1.21.4.

---
+++ Forge/src/main/java/com/natamus/passiveshield/forge/events/ForgeServerEvent.java
@@ -0,0 +1,22 @@
+package com.natamus.passiveshield.forge.events;
+
+import com.natamus.passiveshield.events.ServerEvent;
+import net.minecraft.world.entity.Entity;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
+
+@EventBusSubscriber
+public class ForgeServerEvent {
+	@SubscribeEvent
+	public void onEntityDamageTaken(LivingHurtEvent e) {
+		Entity entity = e.getEntity();
+
+		float damageAmount = e.getAmount();
+		float newAmount = ServerEvent.onEntityDamageTaken(entity.level(), entity, e.getSource(), damageAmount);
+
+		if (newAmount != damageAmount) {
+			e.setAmount(newAmount);
+		}
+	}
+}
