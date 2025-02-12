0d647dd1345ce5191d5bb7b3f2a88907a63fecd6
Rick South <rick@natamus.com>
2025-02-12 23:22:01 +0100

    (v3.7) (Forge & Neo) Removed redundant event register annotations. [+1]

--- NeoForge/src/main/java/com/natamus/passiveshield/neoforge/events/NeoForgeServerEvent.java
+++ NeoForge/src/main/java/com/natamus/passiveshield/neoforge/events/NeoForgeServerEvent.java
@@ -3,10 +3,8 @@ package com.natamus.passiveshield.neoforge.events;
 import com.natamus.passiveshield.events.ServerEvent;
 import net.minecraft.world.entity.Entity;
 import net.neoforged.bus.api.SubscribeEvent;
-import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.neoforge.event.entity.living.LivingIncomingDamageEvent;
 
-@EventBusSubscriber
 public class NeoForgeServerEvent {
 	@SubscribeEvent
 	public static void onEntityDamageTaken(LivingIncomingDamageEvent e) {
